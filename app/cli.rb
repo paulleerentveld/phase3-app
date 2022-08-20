d4dd356ebfc8df92b4af499e6f0f1ac4e68ceafa
paulleerentveld <paul@veldnet.com.au>
2022-08-20 16:56:03 +1000

    getting API and basic save to DB etc working

--- app/cli.rb
+++ app/cli.rb
@@ -1,29 +1,63 @@
 class Cli 
+    
+
     def initialize
         continue = 'y'
         while continue == 'y'
             puts "Welcome to the News CLI App"
             puts "Select an option"
-            puts "1. Get News"
+            puts "1. Get Latest News"
+            puts "2. Saved Articles"
+            puts "3. Exit"
             
             input  = gets.strip
 
             if input == '1'
                 startNews
+            elsif input == '2'
+                puts "get articles"
+            elsif input == '3'
+                return
             else
-            puts "Do you want to continue(y/n)"
-            continue = gets.strip
+                puts "Do you want to continue(y/n)"
+                continue = gets.strip
             end
         end
     end
 
     def startNews
-        News.new.getnews
+        newsContinue = 'y'
+        News.GetNews
+        while newsContinue == 'y'
+            puts "=========================================================================================="
+            puts News.List
+            puts "=========================================================================================="
+            puts "Select a News article to view details"
+            input = gets.strip
+            if input.to_i <= News.List.count 
+                viewarticle = 'y'
+                while viewarticle == 'y'
+                    puts "Title:" + News.All[input.to_i].name
+                    puts "Body:" + News.All[input.to_i].body
+                    puts "1. Save Article"
+                    puts "2. Return"
+                    articleinput = gets.strip
+                    if articleinput == '1'
+                        News.All[input.to_i-1].saveArticle
+                    else 
+                        return
+                    end
+                end
+
+            elsif input.to_i > News.List.count
+                "Please select a valid article number"
+            else
+                puts "Do you want to continue (y/n)"
+                newsContinue = gets.strip
+            end
+        end
     end
 
-    def saveArticle
-
-    end
 
     def getArticles
         puts "Select an option"
--- app/news.rb
+++ app/news.rb
@@ -1,6 +1,54 @@
+require 'open-uri'
+require 'json'
+
 class News
-    def getnews
+
+    attr_accessor :name, :body
+
+    @@all = []
+
+    def initialize(name, body)
+
+        @name = name
+        @body = body
+        #@@all << self
 
     end
+
+    def self.GetNews 
+        url = 'https://newsapi.org/v2/top-headlines?'\
+        'country=au&'\
+        'apiKey=d864aba1ea3141269c84c167e72e5c24'
+
+        request = open(url)
+        response = request.read
+        parsed = JSON(response)
+        parsed['articles'].each.with_index(1) do |article, index|
+            index = News.new(article['title'], article['description'])
+            @@all << index
+        end
+
+    end
+
+    def self.List
+        @@all.map.with_index do |object, index|
+            "#{index} - #{object.name}"
+        end
+    end
+
+    def saveArticle
+        Article.create(:name => self.name, :body => self.body)
+    end
     
+
+    def self.All
+        @@all
+    end
+
+    def showNews
+
+
+    end
+
+
 end
